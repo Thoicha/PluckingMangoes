411bae777fc8be78f7bbbc6acf8f2d837759f632
Iris <69468311+Thoicha@users.noreply.github.com>
2020-11-01 18:45:20 +0530

    Add files via upload

---
+++ Stone.js
@@ -0,0 +1,21 @@
+class Stone{
+    constructor(x,y){
+        var options={
+            'friction': 1,
+            'density': 1.2
+        }
+        this.body = Bodies.circle(x,y,20,options);
+        World.add(world,this.body);
+        this.image = loadImage("stone.png");
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,0,0,50,50);
+        pop();
+    }
+}
